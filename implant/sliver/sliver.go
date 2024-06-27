c7188e14aa49b9fde7b12584f9322de5200968c0
gsmith257 <55564824+gsmith257-cyber@users.noreply.github.com>
2024-06-27 11:34:15 +0900

    Update sliver.go
    Signed-off-by: gsmith257 <55564824+gsmith257-cyber@users.noreply.github.com>

--- implant/sliver/sliver.go
+++ implant/sliver/sliver.go
@@ -66,10 +66,6 @@ var (
 	InstanceID       string
 	connectionErrors = 0
 	ErrTerminate     = errors.New("terminate")
-	// {{if .Config.IsBeacon}}
-	kernel32  = syscall.NewLazyDLL("kernel32.dll")
-	procSleep = kernel32.NewProc("Sleep")
-	// {{end}}
 )
 
 func init() {
@@ -192,12 +188,6 @@ func main() {
 }
 
 // {{if .Config.IsBeacon}}
-func Sleep(d time.Duration) {
-	// Convert duration to milliseconds
-	millis := d / time.Millisecond
-	// Call Windows Sleep API
-	procSleep.Call(uintptr(millis))
-}
 
 func beaconStartup() {
 	// {{if .Config.Debug}}
@@ -225,9 +215,7 @@ func beaconStartup() {
 		// {{if .Config.Debug}}
 		log.Printf("Reconnect sleep: %s", reconnect)
 		// {{end}}
-		//Using WindowsAPI sleep to allow for hooking (sleep masking)
-		Sleep(reconnect * time.Second)
-		//time.Sleep(reconnect)
+		time.Sleep(reconnect)
 	}
 }
 
